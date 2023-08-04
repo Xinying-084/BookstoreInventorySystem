b58304c7eef41e948f7e4f3aed5425c9f87a5c23
XINYING-LI <141435009+Xinying-084@users.noreply.github.com>
2023-08-04 23:45:39 +0800

    init repo

---
+++ src/main/java/com/example/BookstoreInventorySystem/BookstoreInventorySystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.BookstoreInventorySystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreInventorySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreInventorySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BookstoreInventorySystem/BookstoreInventorySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BookstoreInventorySystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreInventorySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
