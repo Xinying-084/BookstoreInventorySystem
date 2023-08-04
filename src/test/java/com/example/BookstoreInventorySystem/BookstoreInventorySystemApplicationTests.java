6672e73df84a4cf592373bc630e79aa9c0fc6cc8
XINYING-LI <141435009+Xinying-084@users.noreply.github.com>
2023-08-05 01:43:22 +0800

    Revert "Update part of function code"
    This reverts commit 1a2d155c7ca95f9cfd149df8206628b4deda10b3.

--- src/test/java/com/example/BookstoreInventorySystem/BookstoreInventorySystemApplicationTests.java
+++ src/test/java/com/example/BookstoreInventorySystem/BookstoreInventorySystemApplicationTests.java
@@ -1,41 +1,13 @@
 package com.example.BookstoreInventorySystem;
 
-import com.example.BookstoreInventorySystem.entity.Book;
-import com.example.BookstoreInventorySystem.repository.BookRepository;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 class BookstoreInventorySystemApplicationTests {
 
-	@Autowired
-    private TestEntityManager entityManager;
-
-    @Autowired
-    private BookRepository bookRepository;
-
-    @Test
-    public void testSaveAndFindBook() {
-        // 创建一个测试用的 Book 实体
-        Book book = new Book();
-        book.setTitle("Sample Book");
-        book.setAuthor("Test Author");
-        book.setIsbn("1234567890");
-        book.setPrice(29.99);
-        book.setQuantityInStock(10);
-
-        // 保存 Book 实体到数据库
-        entityManager.persist(book);
-        entityManager.flush();
-
-        // 从数据库中查询并验证
-        Book foundBook = bookRepository.findById(book.getId()).orElse(null);
-        assertThat(foundBook).isNotNull();
-        assertThat(foundBook.getTitle()).isEqualTo("Sample Book");
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
